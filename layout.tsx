c10cc9071ac89289313e188e4d6297a3de63739f
Nickipicki <n.w.wiestner@gmail.com>
2025-01-13 17:58:49 +0100

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { AuthProvider } from '@/contexts/AuthContext';
 import ClientLayout from '@/components/ClientLayout';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SQQPE0578L"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-SQQPE0578L');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <ClientLayout>
